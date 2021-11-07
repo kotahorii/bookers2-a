3c4cccdf3c93b19ea6b74c3f7b4be0bbf10a2d36
kotahorii <khorii.micron@gmail.com>
2021-11-07 06:31:07 +0000

    ajax DM

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'chats/show'
   devise_for :users
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
   get 'search' => 'searches#search'
+  get 'chat/:id', to: 'chats#show', as: 'chat'
+  resources :chats, only: [:create]
   resources :users,only: [:show,:index,:edit,:update] do
      resources :relationships, only: [:create, :destroy]
      get 'follow' => 'relationships#follow'
